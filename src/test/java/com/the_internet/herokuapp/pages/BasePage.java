cea3d068e2122924339ae451e65461cb6b73213d
Mat Hare <mat_hare@yahoo.com>
2021-06-20 18:58:29 +0100

    Add terminating slash to home page URL

--- src/test/java/com/the_internet/herokuapp/pages/BasePage.java
+++ src/test/java/com/the_internet/herokuapp/pages/BasePage.java
@@ -2,14 +2,13 @@ package com.the_internet.herokuapp.pages;
 
 import java.util.Map;
 
-import static com.the_internet.herokuapp.pages.locators.BaseLocators.getHeader;
-import static com.the_internet.herokuapp.pages.locators.BaseLocators.getSubheader;
+import static com.the_internet.herokuapp.pages.locators.BaseLocators.*;
 
 public class BasePage {
 
     public final String BASE_URL = "https://the-internet.herokuapp.com";
     public final Map<String, String> PAGE_URLS = Map.ofEntries(
-            Map.entry("home", BASE_URL),
+            Map.entry("home", BASE_URL + "/"),
             Map.entry("checkboxes", BASE_URL + "/checkboxes"),
             Map.entry("dropdown", BASE_URL + "/dropdown"),
             Map.entry("dynamic controls", BASE_URL + "/dynamic_controls"),
